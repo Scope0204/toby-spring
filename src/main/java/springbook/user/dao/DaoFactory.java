29c070b3b87912bc0570e85e5698d32218c8b6e2
JunGyeongCho <scope0204@gmail.com>
2025-01-15 22:07:22 +0900

    feat: DaoFactory를 사용하는 애플리케이션 컨텍스트 설정 추가.

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,20 +1,26 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
-
+    @Bean
     public MessageDao messageDao() {
         return new MessageDao(connectionMaker());
     }
-
-    public AccountDao AacountDao() {
+    @Bean
+    public AccountDao accountDao() {
         return new AccountDao(connectionMaker());
     }
 
     // ConnectionMaker 타입 오브젝트를 선정하고 생성하는 중복 로직을 메서드로 분리
-    private ConnectionMaker connectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
 }
